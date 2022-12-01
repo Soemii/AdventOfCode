a7cde0d7249679762b1a3678564945b0791410ee
Sören Michaelis <soeren.michaelis11@gmail.com>
2022-12-01 09:43:39 +0100

    fix flag

--- cmd/main.go
+++ cmd/main.go
@@ -4,20 +4,21 @@ import (
 	"flag"
 	"fmt"
 	"github.com/Soemii/AdventOfCode/internal/util"
-	"github.com/Soemii/AdventOfCode/internal/year2022"
+	"github.com/Soemii/AdventOfCode/internal/year2022/day1"
 	"log"
 )
 
 func main() {
-	year := *flag.String("year", "2022", "set the year")
-	day := *flag.String("day", "1", "set the day")
-	challenge := *flag.String("challenge", "1", "set the challenge")
-	example := *flag.Bool("example", false, "load example data")
-	path := fmt.Sprintf("./data/%v/day%v.txt", year, day)
-	if example {
-		path = fmt.Sprintf("./data/%v/example-%v.txt", year, day)
+	year := flag.String("year", "2022", "set the year")
+	day := flag.String("day", "1", "set the day")
+	challenge := flag.String("challenge", "1", "set the challenge")
+	example := flag.Bool("example", false, "load example data")
+	flag.Parse()
+	path := fmt.Sprintf("./data/%v/day%v.txt", *year, *day)
+	if *example {
+		path = fmt.Sprintf("./data/%v/example-%v.txt", *year, *day)
 	}
-	challengeName := fmt.Sprintf("%v:%v:%v", year, day, challenge)
+	challengeName := fmt.Sprintf("%v:%v:%v", *year, *day, *challenge)
 	fileContent, err := util.ReadFile(path)
 	if err != nil {
 		log.Println("Cannot read file!")
@@ -34,7 +35,8 @@ func main() {
 
 func registerChallenges() map[string]util.Challenge {
 	challenges := make(map[string]util.Challenge)
-	registerChallenge(year2022.Day1_1{}, challenges)
+	registerChallenge(day1.Day1_1{}, challenges)
+	registerChallenge(day1.Day1_2{}, challenges)
 	return challenges
 }
 
